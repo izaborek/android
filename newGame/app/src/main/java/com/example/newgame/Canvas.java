c5467692baff7152a7d8a542f4c0defc63c6af58
Iza Borek <izaborek53@gmail.com>
2019-04-25 10:48:12 +0200

    One letter verification

--- newGame/app/src/main/java/com/example/newgame/Canvas.java
+++ newGame/app/src/main/java/com/example/newgame/Canvas.java
@@ -8,48 +8,81 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Canvas extends View {
 
     Paint paint;
     Path path;
+    ArrayList<Float> listOfPoints;
+    float screenWidth = this.getResources().getDisplayMetrics().widthPixels;
+    float screenHeigth = this.getResources().getDisplayMetrics().heightPixels;
+    boolean ifDraw = false;
+    protected final Logger log = Logger.getLogger(getClass().getName()); //java.util.logging.Logger
+    int pointsCount;
 
-    public Canvas(Context context, AttributeSet attrs) {
+    public Canvas(Context context, AttributeSet attrs,ArrayList<Float> pointsList) {
         super(context, attrs);
         paint = new Paint();
         path = new Path();
+        this.listOfPoints = pointsList;
         paint.setAntiAlias(true);
         paint.setColor(Color.RED); //to change later
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeWidth(15f); //change
-
+        pointsCount = pointsList.size()/2;
     }
+
 
     @Override
     protected void onDraw(android.graphics.Canvas canvas) {
         super.onDraw(canvas);
+        paint.setTextSize(30);
+        paint.setColor(Color.GRAY);
+        paint.setStrokeWidth(40f); //ch
+        for (int i = 0;i < listOfPoints.size();i+=2) {
+            canvas.drawPoint(canvas.getWidth()/2,listOfPoints.get(i+1),paint);
+        }
+        paint.setStrokeWidth(15f); //change
+        paint.setAntiAlias(false);
+        paint.setColor(Color.RED);
         canvas.drawPath(path,paint);
-
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         float xPos = event.getX();
         float yPos = event.getY();
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                path.moveTo(xPos, yPos);
-                return true;
-            case MotionEvent.ACTION_MOVE:
-                path.lineTo(xPos, yPos);
-                break;
-            case MotionEvent.ACTION_UP:
-                break;
-            default:
-                return false;
+        float distX = xPos - screenWidth/2;
+        float distY = yPos - listOfPoints.get(3);
+        double absDist = Math.sqrt(distX * distX + distY * distY);
+        log.log(Level.INFO, "Przetwarzam {0} elementów", distX + " " +distY);
+        if (absDist <= 10) {
+            ifDraw = true;
         }
-        invalidate();
-        return true;
-    }
+        if(ifDraw == true)
+        {
+            log.log(Level.INFO, "Przetwarzam {0} elementów", absDist);
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    path.moveTo(xPos, yPos);
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    path.lineTo(xPos, yPos);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    break;
+                default:
+                    return false;
+            }
+        }
+
+            invalidate();
+            return true;
+        }
+
+
 }
--- newGame/app/src/main/java/com/example/newgame/DrawActivity.java
+++ newGame/app/src/main/java/com/example/newgame/DrawActivity.java
@@ -3,16 +3,31 @@ package com.example.newgame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class DrawActivity extends AppCompatActivity {
 
     Canvas myCanvas;
-
+    ArrayList<Float> listka = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        listka.add((float) 100);
+        listka.add((float) 200);
+        listka.add((float) 100);
+        listka.add((float) 400);
+        listka.add((float) 100);
+        listka.add((float) 500);
+        listka.add((float) 100);
+        listka.add((float) 600);
+        listka.add((float) 100);
+        listka.add((float) 700);
+        listka.add((float) 100);
+        listka.add((float) 800);
+        myCanvas = new Canvas(this, null,listka);
 
-        myCanvas = new Canvas(this, null);
         setContentView(myCanvas);
+
 
     }
 }
