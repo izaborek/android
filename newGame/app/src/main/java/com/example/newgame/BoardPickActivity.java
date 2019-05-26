c5f246c7d8dc7df820e2b1c08a0a9a10d86084cf
Iza Borek <izaborek53@gmail.com>
2019-05-26 14:21:52 +0200

    Add shapes

--- newGame/app/src/main/java/com/example/newgame/BoardPickActivity.java
+++ newGame/app/src/main/java/com/example/newgame/BoardPickActivity.java
@@ -9,20 +9,47 @@ import android.widget.ImageButton;
 
 public class BoardPickActivity extends AppCompatActivity {
 
-    private ImageButton boardOne;
+    private ImageButton boardFirst;
+    private ImageButton boardSecond;
+    private ImageButton boardThird;
+    private ImageButton boardFourth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_pick_activity);
 
-        boardOne = findViewById(R.id.board3Btn);
+        boardFirst = findViewById(R.id.board1Btn);
+        boardSecond = findViewById(R.id.board2Btn);
+        boardThird = findViewById(R.id.board3Btn);
+        boardFourth = findViewById(R.id.board4Btn);
 
-        boardOne.setOnClickListener(new View.OnClickListener() {
+        boardFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BoardPickActivity.this, DrawActivity.class));
+                MainActivity.numberOfBoard = 1;
+            }
+        });
+        boardSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BoardPickActivity.this, DrawActivity.class));
+                MainActivity.numberOfBoard = 2;
+            }
+        });
+        boardThird.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(BoardPickActivity.this, DrawActivity.class));
         MainActivity.numberOfBoard = 3;
+            }
+        });
+        boardFourth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BoardPickActivity.this, DrawActivity.class));
+                MainActivity.numberOfBoard = 4;
             }
         });
     }
--- newGame/app/src/main/java/com/example/newgame/Letter.java
+++ newGame/app/src/main/java/com/example/newgame/Letter.java
@@ -31,12 +31,37 @@ public class Letter {
     public Letter()
     {
         ListOfLetters = new ArrayList<>();
-        ListOfLetters.add("1literaa");
-        ListOfLetters.add("2literab");
-        ListOfLetters.add("3literac");
-        ListOfLetters.add("4literad");
-        ListOfLetters.add("5literae");
-        ListOfLetters.add("6literaf");
+        if(MainActivity.numberOfBoard == 3) {
+            ListOfLetters.add("1literaa");
+            ListOfLetters.add("2literab");
+            ListOfLetters.add("3literac");
+            ListOfLetters.add("4literad");
+            ListOfLetters.add("5literae");
+            ListOfLetters.add("6literaf");
+            ListOfLetters.add("7literag");
+            ListOfLetters.add("8literah");
+            ListOfLetters.add("9literai");
+            ListOfLetters.add("10literaj");
+            ListOfLetters.add("11literak");
+            ListOfLetters.add("12literal");
+            ListOfLetters.add("13literam");
+            ListOfLetters.add("14literan");
+            ListOfLetters.add("15literao");
+            ListOfLetters.add("16literap");
+            ListOfLetters.add("17literar");
+            ListOfLetters.add("18literas");
+            ListOfLetters.add("19literat");
+            ListOfLetters.add("20literau");
+            ListOfLetters.add("21literaw");
+            ListOfLetters.add("22literay");
+            ListOfLetters.add("23literaz");
+        }
+        else if (MainActivity.numberOfBoard == 1)
+        {
+            ListOfLetters.add("1trojkat");
+            ListOfLetters.add("2kwadrat");
+            ListOfLetters.add("3kolo");
+        }
     }
 
     public Letter(String name, List<Float> listOfPoints)
